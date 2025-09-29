57f9917c025090ed5e1b4b55df80c17dd59e0d3c
Bassel Taleb <btalebg@gmail.com>
2025-09-29 09:08:10 -0400

    Initial Commit

---
+++ saraBeauty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  saraBeauty
+//
+//  Created by Bassel Taleb on 9/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ saraBeauty/saraBeautyApp.swift
@@ -0,0 +1,17 @@
+//
+//  saraBeautyApp.swift
+//  saraBeauty
+//
+//  Created by Bassel Taleb on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct saraBeautyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
