bcfaee471ebf8bad8155816a2e6faf8810ece723
Bassel Taleb <btalebg@gmail.com>
2025-09-30 10:29:34 -0400

    home to blome

--- saraBeauty/ContentView.swift
+++ saraBeauty/ContentView.swift
@@ -1,21 +1,72 @@
-//
-//  ContentView.swift
-//  saraBeauty
-//
-//  Created by Bassel Taleb on 9/29/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            if #available(iOS 26, *) {
+                NativeTabView()
+                    .tabViewBottomAccessory {
+                        
+                    }
+            } else {
+                NativeTabView()
+            }
         }
-        .padding()
+    }
+    @ViewBuilder
+    func NativeTabView() -> some View {
+        TabView {
+            Tab.init("Home", systemImage: "house") {
+                NavigationStack {
+                    List {
+                        
+                    }
+                    .navigationTitle(Text("Blome"))
+                }
+            }
+            
+            Tab.init("Library", systemImage: "square.stack") {
+                NavigationStack {
+                    List {
+                        
+                    }
+                    .navigationTitle(Text("Library"))
+                }
+            }
+            
+            Tab.init("Account", systemImage: "person") {
+                NavigationStack {
+                    List {
+                        
+                    }
+                    .navigationTitle(Text("Account"))
+                }
+            }
+            
+            Tab.init("Search", systemImage: "magnifyingglass", role: .search) {
+                NavigationStack {
+                    List {
+                        
+                    }
+                    .navigationTitle("Search")
+                    .searchable(text: $searchText, placement: .toolbar, prompt: Text("Search..."))
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func PlayerInfo(_ size: CGSize) -> some View {
+        HStack(spacing: 12) {
+            RoundedRectangle(cornerRadius: size.height / 4)
+                .fill(.blue.gradient)
+                .frame(width: size.width, height: size.height)
+            
+            VStack(alignment: .leading, spacing: 6) {
+                
+            }
+        }
     }
 }
 
