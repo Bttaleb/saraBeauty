ec7db5c7456cd1c51f49b1840b1cf09c58f42095
Bassel Taleb <btalebg@gmail.com>
2025-10-01 08:39:56 -0400

    homeview, subview

---
+++ saraBeauty/Overview/SubscriptionOV.swift
@@ -0,0 +1,18 @@
+//
+//  SubscriptionOV.swift
+//  saraBeauty
+//
+//  Created by Bassel Taleb on 9/30/25.
+//
+
+import SwiftUI
+
+struct SubscriptionOV: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubscriptionOV()
+}
